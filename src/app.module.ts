3061cfffaec1a645b7583511ebcf130bdf76f4ab
Stanyslav Rykhlikov <stanyslavrykhlikov@gmail.com>
2022-08-19 14:26:21 +0300

    add users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,23 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UsersModule } from './users/users.module';
 
-@Module({})
+@Module({
+  controllers: [],
+  providers: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password:
+        '6409C0513CF816F34864285AC72DC64509B405F9BBE42A37428B916051371B85',
+      database: 'dbr_base',
+      models: [],
+      autoLoadModels: true,
+    }),
+    UsersModule,
+  ],
+})
 export class AppModule {}
