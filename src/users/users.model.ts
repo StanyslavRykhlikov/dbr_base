0af9114150f659d80b91c85cee87022f880c2f6a
Stanyslav Rykhlikov <stanyslavrykhlikov@gmail.com>
2022-08-19 20:48:54 +0300

    attach roles to user

--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,5 +1,13 @@
-import { Column, DataType, Model, Table } from 'sequelize-typescript';
+import {
+  BelongsToMany,
+  Column,
+  DataType,
+  Model,
+  Table,
+} from 'sequelize-typescript';
 import { ApiProperty } from '@nestjs/swagger';
+import { UserRoles } from '../roles/user-roles.model';
+import { Role } from '../roles/roles.model';
 
 interface UserCreationAttrs {
   email: string;
@@ -73,4 +81,7 @@ export class User extends Model<User, UserCreationAttrs> {
     allowNull: true,
   })
   banReason: string;
+
+  @BelongsToMany(() => Role, () => UserRoles)
+  roles: Role[];
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,16 +2,27 @@ import { Injectable } from '@nestjs/common';
 import { User } from './users.model';
 import { InjectModel } from '@nestjs/sequelize';
 import { CreateUserDto } from './dto/create-user.dto';
+import { RolesService } from '../roles/roles.service';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User) private userRepository: typeof User) {}
+  constructor(
+    @InjectModel(User) private userRepository: typeof User,
+    private roleService: RolesService,
+  ) {}
 
   async createUser(dto: CreateUserDto): Promise<User> {
-    return await this.userRepository.create(dto);
+    const role = await this.roleService.getRoleByValue('USER');
+    if (role != null) {
+      const user = await this.userRepository.create(dto);
+      await user.$set('roles', [role.id]);
+      return user;
+    } else {
+      throw 'Ошибка при создании пользователя';
+    }
   }
 
   async getAllUsers(): Promise<User[]> {
-    return await this.userRepository.findAll();
+    return await this.userRepository.findAll({ include: { all: true } });
   }
 }
